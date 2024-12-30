1020b0c972d62bf90a89557071dda046e7b50c37
Brody <brodydwatson@gmail.com>
2024-12-30 13:10:45 +1100

    game now fully works, implementing file openeing to define start pos

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <string>
 #include <chrono>
 #include <thread>
-#include "Window.h"
 
 // My implementation of conweys game of life
 // program is made up of cells
@@ -14,7 +15,7 @@
 int WIDTH = 50;
 int HEIGHT = 25;
 char ALIVE = 'X';
-char DEAD = '.';
+char DEAD = ' ';
 
 void PrintBoard(char** board) {
     
@@ -48,38 +49,59 @@ int cellCount(char** board, int r, int c) {
     return cellCount;
 }
 
-void Mutate(char** board) {
-    
-}
-
 int main()
 {
     // endgoal: implement hashlife?
 
-    //create board
+    //create board alognside copyboard
     char** BOARD = new char* [HEIGHT];
+    char** copyBoard = new char* [HEIGHT];
+
     for (int i = 0; i < HEIGHT; ++i) {
         BOARD[i] = new char[WIDTH];
-    }
-
-    //set to 0
-    for (int y = 0; y < HEIGHT; ++y) {
-        for (int x = 0; x < WIDTH; ++x) {
-            BOARD[y][x] = DEAD;
-        }
-    }
-
-    char** copyBoard = new char* [HEIGHT];
-    for (int i = 0; i < HEIGHT; ++i) {
         copyBoard[i] = new char[WIDTH];
     }
 
+    //fill with dead cells
+
+    // open file now 
+    std::ifstream spawnFile ("./spawns/spawn0.txt");
+    if (spawnFile.is_open()) {
+        std::cout << "opened!" << std::endl;
+    }
+
+    std::string line;
+    int count = 0;
+    while (std::getline(spawnFile, line)) {
+        for (int i = 0; i < line.length(); ++i) {
+            BOARD[count][i] = line[i];
+        }
+        count++;
+    }
+    spawnFile.close();
+
+
+    //for (int y = 0; y < HEIGHT; ++y) {
+    //    for (int x = 0; x < WIDTH; ++x) {
+    //        BOARD[y][x] = DEAD;
+    //    }
+    //}
+    // we want this to be read from a file, 
+    // so we need a way to read from a file that can get the length and width
+    // possibly get width by taking length of first string line and height by
+    // counting until we reach EOF
+    // 
+    // Add starting conditions, cell count/generation improve print speed of screen?
+    // there has to be a better way than a bunch of for loops
+    // 
     //set some random to 1
-    BOARD[14][20] = ALIVE;
-    BOARD[15][20] = ALIVE;
-    BOARD[15][19] = ALIVE;
-    BOARD[16][20] = ALIVE;
-    BOARD[16][21] = ALIVE;
+    //BOARD[14][20] = ALIVE;
+    //BOARD[15][20] = ALIVE;
+    //BOARD[15][19] = ALIVE;
+    //BOARD[16][20] = ALIVE;
+    //BOARD[18][20] = ALIVE;
+    //BOARD[19][20] = ALIVE;
+    //BOARD[16][21] = ALIVE;
     //BOARD[16][21] = ALIVE;
     
     // check on main board, apply changes to copy board.
@@ -90,33 +112,26 @@ int main()
     // wierd pointer stuff
     //std::copy(&BOARD[0][0], &BOARD[0][0]+HEIGHT*WIDTH, &copyBoard[0][0]);
 
-    while (true) {
-        for (int y = 0; y < HEIGHT; ++y) {
-            for (int x = 0; x < WIDTH; ++x) {
-                copyBoard[y][x] = BOARD[y][x];
-            }
-        }
+    //while (true) {
+    //    // copy contents of board to the copy
+    //    for (int y = 0; y < HEIGHT; ++y) { std::copy(BOARD[y],BOARD[y] + WIDTH, copyBoard[y]); }
 
-        for (int i = 0; i < HEIGHT; ++i) {
-            for (int j = 0; j < WIDTH; ++j) {
-                // count cells around current cell position
-                int c = cellCount(BOARD, i, j);
-                if (c < 2 || c > 3) { copyBoard[i][j] = DEAD; }
-                else if (c == 3 && copyBoard[i][j] == DEAD) { copyBoard[i][j] = ALIVE; }
-            }
-        }
+    //    for (int i = 0; i < HEIGHT; ++i) {
+    //        for (int j = 0; j < WIDTH; ++j) {
+    //            // count cells around current cell position
+    //            int c = cellCount(BOARD, i, j);
+    //            if (c < 2 || c > 3) { copyBoard[i][j] = DEAD; }
+    //            else if (c == 3 && copyBoard[i][j] == DEAD) { copyBoard[i][j] = ALIVE; }
+    //        }
+    //    }
 
-        // change these
-        for (int y = 0; y < HEIGHT; ++y) {
-            for (int x = 0; x < WIDTH; ++x) {
-                BOARD[y][x] = copyBoard[y][x];
-            }
-        }
+    //    // change these
+    //    for (int y = 0; y < HEIGHT; ++y) { std::copy(copyBoard[y], copyBoard[y] + WIDTH, BOARD[y]); }
 
-        PrintBoard(BOARD);
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        system("cls");
-    }
+    //    PrintBoard(BOARD);
+    //    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    //    system("cls");
+    //}
 
     //while (true) {
 
